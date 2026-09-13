e12993041eaadb2384eb0215b016ea25ae4a1e61
NemuProject38 <nemu.project3388@gmail.com>
2026-09-13 17:23:00 +0900

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB65897jFthR2hfAjt1Z0NP0P2BAx6SpuA",
+  authDomain: "nemu-fefc2.firebaseapp.com",
+  projectId: "nemu-fefc2",
+  storageBucket: "nemu-fefc2.firebasestorage.app",
+  messagingSenderId: "348415604814",
+  appId: "1:348415604814:web:f9114c17651337b37c2b1f"
+};
